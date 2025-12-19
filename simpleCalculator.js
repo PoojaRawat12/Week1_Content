f3f227d490bf6bfea7ac4be3fffc1364882eee3b
POOJA RAWAT <rawatpooja1228@gmail.com>
2025-12-19 22:00:33 +0530

    Add files via upload
    These are the basic JavaScript programs.

---
+++ simpleCalculator.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = 5;
+let operator = "+";
+if (operator === "+") {
+  console.log(x + y);
+} else if (operator === "-") {
+  console.log(x - y);
+} else if (operator === "*") {
+  console.log(x * y);
+} else if (operator === "/") {
+  console.log(x / y);
+} else {
+  console.log("Invalid operator");
+}
